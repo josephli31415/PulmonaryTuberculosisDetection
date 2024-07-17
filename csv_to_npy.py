95bea9f74e796b184a7f0180228c977a4a04b24e
UrAverageCommie <158373369+DogeSasquatch@users.noreply.github.com>
2024-07-17 16:53:39 -0500

    Update csv_to_npy.py

--- csv_to_npy.py
+++ csv_to_npy.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import numpy as np
+
+csv_file_path = 'path/to/your/file.csv'
+df = pd.read_csv(csv_file_path)
+data_array = df.to_numpy()
+npy_file_path = 'path/to/save/experiment2.npy'
+np.save(npy_file_path, data_array)
+
+print(f"Data saved to {npy_file_path}")
 
